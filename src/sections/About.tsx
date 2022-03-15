5958a386f6127bc0586ef562cc97b30b82fd8d92
kfekairi <kfekairii@gmail.com>
2022-03-15 16:55:20 +0100

    photo added

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,21 +1,27 @@
+import Image from "next/image";
 import React from "react";
 import { SectionHeading } from "../components";
+import profile from "../assets/img/profile.png";
 
 function About() {
   return (
     <div className="about">
       <div className="about__left">
         <SectionHeading />
-        <div className="about__body">
+        <div className="about__left__body">
           <p>
-            I am a Full-Stack developer located in Algiers,Algeria. I am
+            I am a Full-Stack developer located in Algeria (Algiers). I am
             passionated about leveraging my diverse backgrounds to decipher
             challenging problems and create delightful experiences.
           </p>
-          <p>I sharped my skils at Web, Mobile and Embedded Systems.</p>
+          <p>
+            I sharped my skils at Web, Mobile and Embedded Systems development.
+          </p>
         </div>
       </div>
-      <div className="about__right"></div>
+      <div className="about__right">
+        <Image src={profile} width={500} height={500} alt="Me" />
+      </div>
     </div>
   );
 }
