9c2df96913fa0532c2a40f68849d02cf069e7198
kfekairi <dev.kfek@gmail.com>
2022-03-18 10:59:27 +0100

    skills added

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,23 @@
+import Ts from "./ts.svg";
+import Nestjs from "./nestjs.svg";
+import Nodejs from "./nodejs.svg";
+import Postgres from "./postgres.svg";
+import Prisma from "./prisma.svg";
+import Docker from "./docker.svg";
+import Reactjs from "./reactjs.svg";
+import Nextjs from "./nextjs.svg";
+import Scss from "./sass.svg";
+import Redux from "./redux.svg";
+
+export {
+  Ts,
+  Nestjs,
+  Nodejs,
+  Postgres,
+  Prisma,
+  Docker,
+  Reactjs,
+  Nextjs,
+  Scss,
+  Redux,
+};
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -2,8 +2,77 @@ import Image from "next/image";
 import React from "react";
 import { SectionHeading } from "../components";
 import profile from "../assets/img/profile.png";
+import {
+  Docker,
+  Nestjs,
+  Nextjs,
+  Nodejs,
+  Postgres,
+  Prisma,
+  Reactjs,
+  Redux,
+  Scss,
+  Ts,
+} from "../assets/icons";
+
+interface SkillsItemProps {
+  icon: any;
+  title: string;
+}
+
+const SkillsItem = ({ icon, title }: SkillsItemProps) => {
+  return (
+    <li>
+      <Image src={icon} width={32} height={32} alt={title} priority={true} />
+      <span>{title}</span>
+    </li>
+  );
+};
 
 function About() {
+  const skills: SkillsItemProps[] = [
+    {
+      title: "React",
+      icon: Reactjs,
+    },
+    {
+      title: "Nodejs",
+      icon: Nodejs,
+    },
+    {
+      title: "Nextjs",
+      icon: Nextjs,
+    },
+    {
+      title: "Nestjs",
+      icon: Nestjs,
+    },
+    {
+      title: "Scss",
+      icon: Scss,
+    },
+    {
+      title: "Postgres",
+      icon: Postgres,
+    },
+    {
+      title: "Redux",
+      icon: Redux,
+    },
+    {
+      title: "Prisma",
+      icon: Prisma,
+    },
+    {
+      title: "Typescript",
+      icon: Ts,
+    },
+    {
+      title: "Docker",
+      icon: Docker,
+    },
+  ];
+
   return (
     <div className="about">
       <div className="about__left">
@@ -17,10 +86,26 @@ function About() {
           <p>
             I sharped my skils at Web, Mobile and Embedded Systems development.
           </p>
+          <p>Here are a few technologies I’ve been working with recently:</p>
+
+          <ul className="skills-list">
+            {skills.map((item, i) => (
+              <SkillsItem key={i} title={item.title} icon={item.icon} />
+            ))}
+          </ul>
         </div>
       </div>
       <div className="about__right">
-        <Image src={profile} width={500} height={500} alt="Me" />
+        <div className="about__right__image-box">
+          <Image
+            className="about__right__image-box__image"
+            src={profile}
+            priority={true}
+            width={350}
+            height={350}
+            alt="Me"
+          />
+        </div>
       </div>
     </div>
   );
